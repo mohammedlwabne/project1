0ead0d31812dee24c780fa8fc0c9f8237ea8b350
m7mad lw <lwm7mad12@gmail.com>
2022-11-17 18:07:10 +0200

    projec_1

---
+++ app/src/main/java/com/example/project_1/FirebaseStorage.java
@@ -0,0 +1,6 @@
+package com.example.project_1;
+
+public class FirebaseStorage {
+    public static FirebaseStorage getInstance() {
+    }
+}
---
+++ app/src/main/java/com/example/project_1/FragmentLogin.java
@@ -0,0 +1,106 @@
+package com.example.project_1;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentLogin#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentLogin extends Fragment {
+    private FirebaseServices fbs;
+    private EditText password,user;
+    private Button btn;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FragmentLogin() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentLogin.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentLogin newInstance(String param1, String param2) {
+        FragmentLogin fragment = new FragmentLogin();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        connect();
+    }
+    private void connect(){
+        user=getView().findViewById(R.id.EmailLogin);
+        password=getView().findViewById(R.id.PasswordLogin);
+        btn=getView().findViewById(R.id.LoginMain);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pass=password.getText().toString();
+                String usr=user.getText().toString();
+                fbs.getAuth().signInWithEmailAndPassword(pass, usr)
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // PUT YOUR CODE HERE
+                                } else {
+                                    // PUT YOUR CODE HERE
+                                }
+                            }
+                        });
+            }
+        });
+       
+
+        }
+
+    }
